48226663ade4b88ee5100bb4f0c5c07c4b120885
Michelle Burton <msmichelleburton@yahoo.com>
2025-10-26 12:58:30 -0500

    fix active

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const filterButtons = document.querySelectorAll(".filter-btn");
     const designItems = document.querySelectorAll(".design-item");
-    const links = document.querySelectorAll(".navbar .nav-link");
-
-    console.log(links)
-
     filterButtons.forEach(button => {
         button.addEventListener("click", () => {
             // Remove active class from all buttons
@@ -23,4 +19,32 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     });
+
+
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    const navLinks = document.querySelectorAll(".navbar .nav-link");
+      console.log('Nav Links after delay:', navLinks);
+      // Get the current page filename (e.g. "about.html")
+      const currentPage = window.location.pathname.split("/").pop();
+      navLinks.forEach(link => {
+            
+        // Remove any existing active class
+        link.classList.remove("active");
+
+        // Extract the href filename from the link
+        const linkPage = link.getAttribute("href").split("/").pop();
+
+        // Add 'active' if it matches the current page
+        if (linkPage === currentPage) {
+        link.classList.add("active");
+        }
+    });
+
+  }, 100); // wait 0.3 seconds
+});
+
+
+
