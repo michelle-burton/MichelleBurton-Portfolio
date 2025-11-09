80baa8563d9f144aca8c9985c015313215ac1a75
Michelle Burton <msmichelleburton@yahoo.com>
2025-11-09 11:48:22 -0600

    fix nav

--- new-site/src/components/Header.jsx
+++ new-site/src/components/Header.jsx
@@ -1,8 +1,28 @@
 
 export default function Header() {
     return (
-        <header className="bt-light border-bottom p-3">
-            <h1>head</h1>
+        <header>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark border-bottom sticky-top">
+                <div className="container">
+                <a className="navbar-brand fw-semibold gradient" href="/index.html">
+                    Michelle&nbsp;Burton<span className="text-light">'s&nbsp;Portfolio</span>
+                </a>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
+                        aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-end" id="mainNav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item"><a className="nav-link" href="/index.html">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/portfolio.html">Portfolio</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/about.html">About</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
         </header>
     );
 }
