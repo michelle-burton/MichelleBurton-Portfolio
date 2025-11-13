08e2c79d11a2a5cbcf18e0dab16d883e1e8ba50a
Michelle Burton <msmichelleburton@yahoo.com>
2025-11-13 12:43:16 -0600

    fixed import / use issue with scss

--- new-site/src/main.jsx
+++ new-site/src/main.jsx
@@ -1,11 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './css/normalize.css'
-import './css/style.css'
+// import './css/normalize.css'
+import './styles/main.scss';
+// import './css/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
