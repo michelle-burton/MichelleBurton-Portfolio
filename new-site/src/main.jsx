c2e56aa53159f572d27e33730278dab8a941d6a3
Michelle Burton <msmichelleburton@yahoo.com>
2025-11-18 12:12:03 -0600

    added styles.scss

--- new-site/src/main.jsx
+++ new-site/src/main.jsx
@@ -1,6 +1,3 @@
-scss// import 'bootstrap/dist/css/bootstrap.min.css'
-
-
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
@@ -9,7 +6,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './css/normalize.css'
 import './styles/main.scss';
-import './css/style.scss'
+import './styles/styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
