b2629b41db4adaafc227dec69c2377fb3f81a61e
Michelle Burton <msmichelleburton@yahoo.com>
2025-11-18 12:21:00 -0600

    push styles

--- new-site/src/main.jsx
+++ new-site/src/main.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './css/normalize.css'
-// import './styles/main.scss';
-import './styles/styles.scss'
+import './styles/main.scss';
+// import './styles/styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
