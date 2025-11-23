5438ee4698e46869083f1f6a882120379042453e
Michelle Burton <msmichelleburton@yahoo.com>
2025-11-23 11:26:20 -0600

    add link homepage

--- new-site/src/pages/Home.jsx
+++ new-site/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
             <div className="hero-content">
                 <h1>Where Design Meets Function</h1>
                 <p>I bridge aesthetics and technology to deliver intuitive, high-performing web experiences.</p>
-                <a href="portfolio.html" className="cta-button">Discover More</a>
+                <a href="/portfolio" className="cta-button">Discover More</a>
             </div>
         </div>
     )
