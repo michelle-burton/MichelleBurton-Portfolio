78546a83bec3a02b04e1cab99b74848004893e37
Michelle Burton <msmichelleburton@yahoo.com>
2025-11-23 11:55:16 -0600

    Fix img paths

--- new-site/src/pages/Portfolio.jsx
+++ new-site/src/pages/Portfolio.jsx
@@ -10,7 +10,7 @@ function Portfolio() {
                 <div class="col">
                     <div class="design-item" data-category="code">
                         <div class="col">
-                        <a href="https://stock-tracker-2tc9.onrender.com/Total%20Return%20Tracker/index.html" target="_blank"><img src="/img/Thumb_09.png" alt="Stock Tracker"/></a>
+                        <a href="https://stock-tracker-2tc9.onrender.com/Total%20Return%20Tracker/index.html" target="_blank"><img src="images/Thumb_09.png" alt="Stock Tracker"/></a>
                         <div class="overlay"><a href="https://stock-tracker-2tc9.onrender.com/Total%20Return%20Tracker/index.html" target="_blank">View Design</a></div>
                     </div>
                     </div> 
@@ -36,32 +36,32 @@ function Portfolio() {
                 </div>
                 <div class="col">
                 <div class="card design-item" data-category="code">
-                    <a href="dog-viewer/index.html"><img src="/img/Thumb_08.png" class="img-fluid" alt="Dog Viewer API"/></a>
+                    <a href="dog-viewer/index.html"><img src="images/Thumb_08.png" class="img-fluid" alt="Dog Viewer API"/></a>
                     <div class="overlay"><a href="dog-viewer/index.html">View Design</a></div>
                     </div>
                 </div>
           
                  <div class="col">
                     <div class="design-item" data-category="illustrations">
-                        <a href="design/index.html?id=3"><img src="/img/Thumb_03.png" alt="Creative Illustration"/></a>
+                        <a href="design/index.html?id=3"><img src="images/Thumb_03.png" alt="Creative Illustration"/></a>
                         <div class="overlay"><a href="design/index.html?id=3">View Design</a></div>
                     </div>
                 </div>
                  <div class="col">
                     <div class="design-item" data-category="ui-kits">  
-                    <a href="design/index.html?id=6"><img src="/img/Thumb_06.png" alt="Mobile UI Kit"/></a>
+                    <a href="design/index.html?id=6"><img src="images/Thumb_06.png" alt="Mobile UI Kit"/></a>
                     <div class="overlay"><a href="design/index.html?id=6">View Design</a></div>
                     </div>
                 </div>
                  <div class="col">
                     <div class="card design-item"  data-category="code">
-                            <a href="code-task-list/index.html"><img src="/img/Thumb_07.png" class="img-fluid" alt="Code Project"/></a>
+                            <a href="code-task-list/index.html"><img src="images/Thumb_07.png" class="img-fluid" alt="Code Project"/></a>
                             <div class="overlay"><a href="code-task-list/index.html">View Feature</a></div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card design-item"  data-category="illustrations">
-                            <a href="design/index.html?id=4"><img src="/img/Thumb_04.png" class="img-fluid" alt="Illustration"/></a>
+                            <a href="design/index.html?id=4"><img src="images/Thumb_04.png" class="img-fluid" alt="Illustration"/></a>
                             <div class="overlay"><a href="design/index.html?id=4">View Feature</a></div>
                     </div>
                 </div>
