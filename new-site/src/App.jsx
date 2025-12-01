3ce8bbbeabf75792d1f8c5a0731c39ce913263ef
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-01 17:06:44 -0600

    added route

--- new-site/src/App.jsx
+++ new-site/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Contact from "./pages/Contact.jsx";
 import Portfolio from "./pages/Portfolio.jsx";
+import Grid from "./pages/grid.jsx";
 
 
 
@@ -18,6 +19,7 @@ function App() {
                 <Route path="/about" element={<About />} /> 
                 <Route path="/portfolio" element={<Portfolio />} /> 
                 <Route path="/contact" element={<Contact />} /> 
+                <Route path="/projects" element={<Grid />} />
             </Routes>
             <Footer />
         </Router>
