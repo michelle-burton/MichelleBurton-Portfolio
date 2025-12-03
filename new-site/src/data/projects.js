66ec66a60d6c6dc0b2bd0db744176e42ac115791
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-03 08:08:33 -0600

    add images

--- new-site/src/data/projects.js
+++ new-site/src/data/projects.js
@@ -1,13 +1,13 @@
 // src/data/projects.js
 export const projects = [
   {
-    id: "puppy-palace",
+    id: "Dog Browser",
     title: "Dog Browser React App",
     type: "code", // "code" | "design" | "case-study" etc.
     description: "Adoptable pups browser with filters, favorites, and a playful UI.",
     thumbnail: "/images/Thumb_08.png",
-    liveUrl: "https://your-live-url.com/puppy-palace",
-    repoUrl: "https://github.com/michelleburton/puppy-palace",
+    liveUrl: "https://puppy-palace-react-app.onrender.com/",
+    repoUrl: "https://github.com/michelle-burton/Dog-Viewer",
   },
   {
     id: "stock-dashboard",
@@ -15,12 +15,12 @@ export const projects = [
     type: "code",
     description: "LLM-assisted dashboard to monitor stocks and dividend income.",
     thumbnail: "/images/Thumb_09.png",
-    liveUrl: "https://your-live-url.com/stock-dashboard",
-    repoUrl: "https://github.com/michelleburton/ai-stock-dashboard",
+    liveUrl: "https://stock-tracker-2tc9.onrender.com/Total%20Return%20Tracker/index.html",
+    repoUrl: "https://github.com/michelle-burton/stock-dashboard",
   },
   {
-    id: "figma-camera-store",
-    title: "Camera Store Homepage",
+    id: "figma",
+    title: "Figma Design",
     type: "design",
     description: "Figma e-commerce layout with hero, promo banner, and product grid.",
     thumbnail: "/images/Thumb_06.png",
@@ -28,11 +28,11 @@ export const projects = [
     repoUrl: null,
     },
     {
-    id: "puppy-palace",
-    title: "Puppy Palace React App",
-    type: "code", // "code" | "design" | "case-study" etc.
+    id: "AI-Art 01",
+    title: "AI Art",
+    type: "design", // "code" | "design" | "case-study" etc.
     description: "A fun, open source React app where anyone can add their pup to a shared gallery 🐶✨",
-    thumbnail: "/images/Thumb_10.png",
+    thumbnail: "/images/Thumb_01.png",
     liveUrl: "https://your-live-url.com/puppy-palace",
     repoUrl: "https://github.com/michelleburton/puppy-palace",
   },
@@ -46,7 +46,7 @@ export const projects = [
     repoUrl: "https://github.com/michelleburton/ai-stock-dashboard",
   },
   {
-    id: "figma-camera-store",
+    id: "AI Art",
     title: "AI-Generation Design",
     type: "design",
     description: "AI e-commerce layout with hero, promo banner, and product grid.",
