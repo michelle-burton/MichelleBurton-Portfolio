d13198baddc0068bbc1a1a82690b563b2f37234e
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-06 07:49:45 -0600

    add new link

--- new-site/src/components/Header.jsx
+++ new-site/src/components/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
                 <div className="collapse navbar-collapse justify-content-end" id="mainNav">
                     <ul className="navbar-nav">
                     <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/portfolio">Portfolio</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/project">Portfolio</a></li>
                     <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
                     <li className="nav-item"><a className="nav-link" href="/contact">Contact</a></li>
                     </ul>
