9fea28ec317bd9c55b963ab358efa73735373ad2
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-07 11:46:25 -0600

    add filter

--- new-site/src/pages/Projects.jsx
+++ new-site/src/pages/Projects.jsx
@@ -1,7 +1,9 @@
 // src/pages/Grid.jsx
 import { projects } from "../data/projects";
+import { useState } from "react";
 
 export default function Projects() {
+    const [filter, setFilter] = useState("all")
     
 //     const filtered = projects.filter(p =>
 //   filter === "all" ? true : p.type === filter
