1de6e37e169356e40e95aa4de73ab72e220fdc68
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-11 13:06:06 -0600

    add filterbar

--- new-site/src/components/FilterBar.jsx
+++ new-site/src/components/FilterBar.jsx
@@ -1,1 +1,14 @@
 // TODO add code
+import { useState } from "react";
+
+export default function FilterBar({ categories, onFilter }) {
+  const [active, setActive] = useState("All");
+
+  const handleClick = (category) => {
+    setActive(category);
+    onFilter(category);
+  };
+
+  return ()
+
+}
