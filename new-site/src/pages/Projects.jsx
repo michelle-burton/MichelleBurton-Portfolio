9628864c1e08a3f0283be2c23d30cde8f40886fb
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-12 08:00:19 -0600

    comment out

--- new-site/src/pages/Projects.jsx
+++ new-site/src/pages/Projects.jsx
@@ -20,49 +20,7 @@ export default function Projects() {
           </div>
 
         <div className="projects-grid">
-                 {filtered.map(p => <ProjectCard key={p.id} data={p} />)} 
-        {projects.map((project) => (
-          <article key={project.id} className="project-card">
-            {/* Thumbnail */}
-            <div className="project-thumb">
-              <img
-                src={project.thumbnail}
-                alt={`${project.title} thumbnail`}
-              />
-            </div>
 
-            {/* Content */}
-            <div className="project-body">
-              <h2 className="project-name">{project.title}</h2>
-              <p className="project-type">{project.type}</p>
-              <p className="project-description">{project.description}</p>
-
-              {/* Buttons */}
-              <div className="project-actions">
-                {project.liveUrl && (
-                  <a
-                    href={project.liveUrl}
-                    className="btn btn-primary"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View Project
-                  </a>
-                )}
-                {project.repoUrl && (
-                  <a
-                    href={project.repoUrl}
-                    className="btn btn-outline"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View Code
-                  </a>
-                )}
-              </div>
-            </div>
-          </article>
-        ))}
       </div>
     </section>
   );
