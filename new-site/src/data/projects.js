ce511311b0c25918b887229fc1692c8dd5367d35
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-31 06:25:26 -0600

    add url

--- new-site/src/data/projects.js
+++ new-site/src/data/projects.js
@@ -70,7 +70,7 @@ export const projects = [
     description: "AI e-commerce layout with hero, promo banner, and product grid.",
     thumbnail: "/images/Thumb_04.png",
     liveUrl: "https://github.com/michelle-burton/Dog-Viewer",
-    repoUrl: null,
+    repoUrl: "https://github.com/michelle-burton/Dog-Viewer",
     },
                 {
     id: "AI Art 3",
