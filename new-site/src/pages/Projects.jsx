4e73a5dc8bcc4caab3d9705447b652f4212c45b7
Michelle Burton <msmichelleburton@yahoo.com>
2026-01-07 10:28:01 -0600

    add grid

--- new-site/src/pages/Projects.jsx
+++ new-site/src/pages/Projects.jsx
@@ -1,25 +1,38 @@
 // src/pages/Grid.jsx
 import { projects } from "../data/projects";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Projects() {
-    const [filter, setFilter] = useState("all")
+    // const [filter, setFilter] = useState("all")
     
-    const filtered = projects.filter(p =>
-        filter === "all" ? true : p.type === filter
-    );
+    // const filtered = projects.filter(p =>
+    //     filter === "all" ? true : p.type === filter
+    // );
   return (
     <section className="projects-section container">
           <h1 className="projects-title">Projects</h1>
-          // TODO: add filtering loginc for project types
-          <div>
+
+          {/* <div>
             <button onClick={() => setFilter("all")}>All</button>
             <button onClick={() => setFilter("code")}>Code</button>
             <button onClick={() => setFilter("design")}>Design</button>
 
-          </div>
+          </div> */}
 
-        <div className="projects-grid">
+          <div className="projects-grid">
+              {projects.map((project) => (
+                  <div className="project-card">
+                <a
+                href={project.liveUrl}
+                target="_blank"
+                rel="noreferrer"
+                className="project-thumb"
+                aria-label={`Open live site for ${project.title}`}
+                >
+                <img src={project.thumbnail} alt={`${project.title} thumbnail`} />
+                </a>
+                </div>
+              ))}
 
       </div>
     </section>
