ea6b6eb2ddc5685ef95d89200ffbf75d25e5dbaa
Michelle Burton <msmichelleburton@yahoo.com>
2026-01-28 12:09:21 -0600

    add figma layout project

--- src/data/projects.js
+++ src/data/projects.js
@@ -118,6 +118,15 @@ export const projects = [
     thumbnail: "/images/Thumb_04.png",
     liveUrl: 'https://github.com/michelle-burton/ai-art-asset/blob/main/img/Thumb_04.png?raw=true',
     repoUrl: 'https://github.com/michelle-burton/ai-art-asset/',
-    }
+    },
+    {
+    id: "Figma3",
+    title: "Portfolio Homepage UX & UI Design",
+    type: "design",
+    description: "Responsive homepage layout for michelle-burton.com, designed in Figma with a focus on layout systems, visual balance, and developer-ready structure.",
+    thumbnail: "/images/Thumb_15.png",
+    liveUrl:'https://www.figma.com/design/vwh3ngQ0dh2HU1uFccH2Jq/michelle-burton.com?node-id=0-1&m=dev&t=OUpBmZFgdm6AeN89-1',
+    repoUrl: "https://www.figma.com/design/vwh3ngQ0dh2HU1uFccH2Jq/michelle-burton.com?node-id=0-1&m=dev&t=OUpBmZFgdm6AeN89-1",
+    },
 
 ];
